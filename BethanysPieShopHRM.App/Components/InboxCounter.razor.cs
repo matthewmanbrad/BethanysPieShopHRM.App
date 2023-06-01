84579aa609921f07ec1082d77f9e8b605d85d175
Brad Matthewman <mattb549@firstactuarial.co.uk>
2023-06-01 15:43:04 +0100

    get map working

---
+++ BethanysPieShopHRM.App/Components/InboxCounter.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.App.Components
+{
+    public partial class InboxCounter
+    {
+        [Inject]
+        public ApplicationState ApplicationState { get; set; }
+        private int MessageCount;
+
+        protected override void OnInitialized()
+        {
+            MessageCount = new Random().Next(10);
+            ApplicationState.NumberOfMessages = MessageCount;
+        }
+    }
+}
--- BethanysPieShopHRM.App/Pages/EmployeeOverview.razor.cs
+++ BethanysPieShopHRM.App/Pages/EmployeeOverview.razor.cs
@@ -1,15 +1,26 @@
 ﻿using BethanysPieShopHRM.App.Models;
+using BethanysPieShopHRM.App.Services;
 using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
 
 namespace BethanysPieShopHRM.App.Pages
 {
 	public partial class EmployeeOverview
 	{
-		public List<Employee> Employees { get; set; } = default!;
+		[Inject]
+        public IEmployeeDataService? EmployeeDataService { get; set; }			
+        public List<Employee> Employees { get; set; } = default!;
+		private Employee? _selectedEmployee;
+		private string Title = "Employee overview";
 
-		protected override void OnInitialized()
+		protected override async Task OnInitializedAsync()
 		{
-			Employees = MockDataService.Employees;
+			Employees = (await EmployeeDataService.GetAllEmployees()).ToList();
+		}
+
+		public void ShowQuickViewPopup(Employee selectedEmployee)
+		{
+			_selectedEmployee = selectedEmployee;
 		}
 	}
 }
